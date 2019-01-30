cc6fee44c3956131b400cb04a579599bfdab1a03
root <root@DESKTOP-GDQ7OH9.localdomain>
2019-01-30 14:29:51 -0500

    added flexbox styles and fonts

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        React Calculator
         <Screen input={this.state.calculation} />
         <Buttons>
           <Button onClick={this.handleClick} label="C" value="clear" />
--- src/Buttons.js
+++ src/Buttons.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Buttons extends Component {
     render(){
         return(
-            <div className="Buttons">{this.props.children}</div>
+            <div className="buttons">{this.props.children}</div>
         )
     }
 }
