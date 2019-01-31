3a6897b33d4ed5b38fdd00ec5bce450d9edaf83a
root <root@DESKTOP-GDQ7OH9.localdomain>
2019-01-31 14:24:34 -0500

    added error message marquee, added result display

--- src/App.js
+++ src/App.js
@@ -9,39 +9,108 @@ import Button from './Button';
 class App extends Component {
   constructor(){
     super()
-    this.state = {calculation: []}
+    this.state = {
+      calculation: [],
+      result: 0,
+    }
   }
 
   calculate = () => {
-    let result = this.state.calculation.join('')
-    if (result) {
-      result = math.eval(result)
-      result = math.format(result, {precision: 14})
-      result = String(result)
+    let answer = this.state.calculation.join('')
+    if (answer && this.state.result === 0) {
+      answer = math.eval(answer)
+      answer = math.format(answer, {precision: 14})
+      answer = String(answer)
       this.setState({
-        calculation: [result],
+        result: answer,
+        calculation: [],
+      })
+    } else if (answer && this.state.result !== 0){
+      answer = String(this.state.result) + answer
+      answer = math.eval(answer)
+      answer = math.format(answer, {precision: 14})
+      answer = String(answer)
+      this.setState({
+        result: answer,
+        calculation: [],
       })
     }
   }
 
   handleClick = e => {
     const value = e.target.getAttribute('data-value')
+    const current = this.state.calculation
+    const result = this.state.result
+    let typr = null
+    
+    const errorMessage = document.getElementById('error')
+
+    try {
+      typr = math.eval(value)
+      console.log('eval: ' + typr)
+    } catch {
+      console.log('eval failed')
+    }
+    const typrOf = typeof(typr)
+    
     switch(value) {
       case 'clear':
         this.setState({
           calculation: [],
         })
         break
+      case 'clearall':
+        this.setState({
+          calculation: [],
+          result: 0
+        })
+        break
       case 'equal':
         this.calculate()
         break
+      case '/':
+      case '*':
+      case '+':
+      case '-':
+      case '.':
+        if(
+          current[current.length - 1] === '/' ||
+          current[current.length - 1] === '*' ||
+          current[current.length - 1] === '+' ||
+          current[current.length - 1] === '-' ||
+          current[current.length - 1] === '.'
+        ){
+          errorMessage.innerHTML = "ERR: only numbers can be input after an operator/decimal"
+        } else if (result !== 0 && value === '.'){
+          errorMessage.innerHTML = "ERR: only operators can continue this calculation"
+        } else {
+          const newOperations = update(current, {
+            $push: [value],
+          })
+          this.setState({
+            calculation: newOperations,
+          })
+        }
+        break
       default:
-        const newOperations = update(this.state.calculation, {
-          $push: [value],
-        })
-        this.setState({
-          calculation: newOperations,
-        })
+        if (result === 0){
+          const newOperations = update(current, {
+            $push: [value],
+          })
+          this.setState({
+            calculation: newOperations,
+          })
+          errorMessage.innerHTML = ""
+        } else if (result !== 0 && typrOf === 'number' && current.length===0){
+          errorMessage.innerHTML = "ERR: only operators can continue this calculation"
+        }  else {
+          const newOperations = update(current, {
+            $push: [value],
+          })
+          this.setState({
+            calculation: newOperations,
+          })
+        }
         break
     }
   }
@@ -50,6 +119,8 @@ class App extends Component {
     return (
       <div className="App">
         React Calculator
+        <marquee behavior="scroll" direction="left"id="error"></marquee>
+        <p class="result">{this.state.result}</p>
         <Screen input={this.state.calculation} />
         <Buttons>
           <Button onClick={this.handleClick} label="C" value="clear" />
@@ -58,21 +129,21 @@ class App extends Component {
           <Button onClick={this.handleClick} label="1" value="1" />
           <Button onClick={this.handleClick} label="0" value="0" />
 
-          <Button onClick={this.handleClick} label="/" value="/" />
+          <Button onClick={this.handleClick} label="ce" value="clearall" />
           <Button onClick={this.handleClick} label="8" value="8" />
           <Button onClick={this.handleClick} label="5" value="5" />
           <Button onClick={this.handleClick} label="2" value="2" />
           <Button onClick={this.handleClick} label="." value="." />
 
-          <Button onClick={this.handleClick} label="x" value="*" />
+          <Button onClick={this.handleClick} label="/" value="/" />
           <Button onClick={this.handleClick} label="9" value="9" />
           <Button onClick={this.handleClick} label="6" value="6" />
           <Button onClick={this.handleClick} label="3" value="3" />
-          <Button label="" value="null" />
+          <Button onClick={this.handleClick} label="=" value="equal" />
 
-          <Button onClick={this.handleClick} label="-" value="-" />
+          <Button onClick={this.handleClick} label="x" value="*" />
+          <Button onClick={this.handleClick} label="-" size="2" value="-" />
           <Button onClick={this.handleClick} label="+" size="2" value="+" />
-          <Button onClick={this.handleClick} label="=" size="2" value="equal" />
         </Buttons>
       </div>
     );
